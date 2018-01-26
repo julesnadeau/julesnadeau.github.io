94192e14a786a9659197c117614585c76e99e265
Paul-Louis <paullouismasca@gmail.com>
2018-01-26 10:11:30 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,145 @@
+var __config__ = {
+	elements: [
+		'<li><a href="" style="outline: 0; text-decoration: none;">JULES NADEAU</a></li>', 
+		'<li class="blank-element"></li>', 
+		'<li class="blank-element"></li>', 
+		'<li style="padding: 0;">Galerie</li>', 
+		{
+			name: '/001', 
+			images: [
+				"./ressources/img/001/thumbnail/001.jpg", 
+				"./ressources/img/001/thumbnail/002.jpg", 
+				"./ressources/img/001/thumbnail/003.jpg", 
+				"./ressources/img/001/thumbnail/004.jpg"
+			], 
+			description: 'Illford HP5+<br />Leica M6<br />Août 2017'
+		}, 
+		{
+			name: '/002', 
+			images: [
+				"./ressources/img/002/thumbnail/001.jpg", 
+				"./ressources/img/002/thumbnail/002.jpg", 
+				"./ressources/img/002/thumbnail/003.jpg", 
+				"./ressources/img/002/thumbnail/004.jpg", 
+				"./ressources/img/002/thumbnail/005.jpg"
+			], 
+			description: 'Agfa 400<br />Leica M6<br />Septembre 2017'
+		}, 
+		{
+			name: '/003', 
+			images: [
+				"./ressources/img/003/thumbnail/001.jpg", 
+				"./ressources/img/003/thumbnail/002.jpg", 
+				"./ressources/img/003/thumbnail/003.jpg", 
+				"./ressources/img/003/thumbnail/004.jpg", 
+				"./ressources/img/003/thumbnail/005.jpg", 
+				"./ressources/img/003/thumbnail/006.jpg", 
+				"./ressources/img/003/thumbnail/007.jpg"
+			], 
+			description: 'Agfa 400<br />Leica M6<br />Octobre 2017'
+		}, 
+		{
+			name: '/004', 
+			images: [
+				"./ressources/img/004/thumbnail/001.jpg", 
+				"./ressources/img/004/thumbnail/002.jpg", 
+				"./ressources/img/004/thumbnail/003.jpg", 
+				"./ressources/img/004/thumbnail/004.jpg"
+			], 
+			description: 'Agfa 400<br />Leica M6<br />Octobre 2017'
+		}, 
+		{
+			name: '/005', 
+			images: [
+				"./ressources/img/005/thumbnail/001.jpg", 
+				"./ressources/img/005/thumbnail/002.jpg", 
+				"./ressources/img/005/thumbnail/003.jpg", 
+				"./ressources/img/005/thumbnail/004.jpg", 
+				"./ressources/img/005/thumbnail/005.jpg"
+			], 
+			description: 'Illford Delta 100<br />Leica M6<br />Octobre 2017'
+		}, 
+		{
+			name: '/006', 
+			images: [
+				"./ressources/img/006/thumbnail/001.jpg", 
+				"./ressources/img/006/thumbnail/002.jpg", 
+				"./ressources/img/006/thumbnail/003.jpg"
+			], 
+			description: 'Illford Delta 100<br />Leica M6<br />Octobre 2017'
+		}, 
+		{
+			name: '/007', 
+			images: [
+				"./ressources/img/007/thumbnail/001.jpg", 
+				"./ressources/img/007/thumbnail/002.jpg", 
+				"./ressources/img/007/thumbnail/003.jpg", 
+				"./ressources/img/007/thumbnail/004.jpg", 
+				"./ressources/img/007/thumbnail/005.jpg"
+			], 
+			description: 'Fuji Neopan Acros 100<br />Leica M6<br />Novembre 2017'
+		}, 
+		{
+			name: '/008', 
+			images: [
+				"./ressources/img/008/thumbnail/001.jpg", 
+				"./ressources/img/008/thumbnail/002.jpg", 
+				"./ressources/img/008/thumbnail/003.jpg", 
+				"./ressources/img/008/thumbnail/004.jpg", 
+				"./ressources/img/008/thumbnail/005.jpg"
+			], 
+			description: 'Illford Delta 100 (à 400)<br />Leica M6<br />Décembre 2017'
+		}, 
+		{
+			name: '/009', 
+			images: [
+				"./ressources/img/009/thumbnail/001.jpg", 
+				"./ressources/img/009/thumbnail/002.jpg", 
+				"./ressources/img/009/thumbnail/003.jpg"
+			], 
+			description: 'Illford Hp5 400<br />Leica M6<br />Décembre 2017'
+		}, 
+		{
+			name: '/010', 
+			images: [
+				"./ressources/img/010/thumbnail/001.jpg", 
+				"./ressources/img/010/thumbnail/002.jpg", 
+				"./ressources/img/010/thumbnail/003.jpg", 
+				"./ressources/img/010/thumbnail/004.jpg", 
+				"./ressources/img/010/thumbnail/005.jpg"		
+			], 
+			description: 'Efke R50<br />Hasselblad 500c/m<br />Décembre 2017'
+		}, 
+		{
+			name: '/011', 
+			images: [
+				"./ressources/img/011/thumbnail/001.jpg", 
+				"./ressources/img/011/thumbnail/002.jpg"
+			], 
+			description: 'Lucky 400<br />Hasselblad 500 c/m<br />Décembre 2017'
+		}, 
+		'<li class="blank-element"></li>', 
+		{
+			name: 'À propos', 
+			images: [], 
+			description: `<p style="text-align:left;right:0;line-height:200%;width:90%;">
+	Salut!<br />
+	<br />
+	Étudiant montréalais de 18 ans et photographe autodidacte, 
+	j’apporte ma caméra partout où je vais. 
+	Je passe la majeure partie de mon temps libre à développer dans mon sous sol. 
+	Ce site regroupe chacun de mes rouleaux. 
+	Si vous avez des questions ou désirez tout simplement dire bonjour, 
+	n’hésitez pas à me contacter!<br />
+	<br />
+	Pour toutes questions/requêtes, 
+	contactez moi au:<br />
+	<a href="mailto:julesjulesnadeau@yahoo.ca">
+		julesjulesnadeau@yahoo.ca
+	</a>
+</p>`
+		}, 
+		'<li class="blank-element"></li>', 
+		'<li><a href="https://www.instagram.com/pangae.a/" target="_blank" class="ms-text" style="font-size: smaller;">Instagram</a></li>'
+	]
+};
