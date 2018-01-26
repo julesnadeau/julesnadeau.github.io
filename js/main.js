b7302e7933415896abb9d33529bef9cd764ac68d
Paul-Louis <paullouismasca@gmail.com>
2018-01-26 10:25:04 -0500

    Compressed the file for faster loading

--- js/main.js
+++ js/main.js
@@ -2,15 +2,7 @@ const app = {
 	build: (a, b = d => void(d), c = undefined) => {
 		let nav = [], container = [], oc = 0;
 
-		if (a.elements) a
-			.elements
-			.forEach(e => {
-				if (typeof e == 'object') {
-					nav = nav.concat('<li class="navnav' + (oc == 0 ? ' current' : '') + '" id="' + (e.name || '') + '"><a onclick="divShow(\'' + (e.name || '') + '\')">' + (e.name || '') + '</a></li>');
-					container = container.concat('<div id="' + (e.name || '') + '" class="element' + (oc == 0 ? '' : ' dispf') + '">' + (r => r.map(d => '<img data-src="' + d.replace(/(\/thumbnail\/)/g, '/') + '" src="' + d + '" class="lazyload" />').join('\n'))(e.images || []) + '<p><span class="center info">' + (e.description || '') + '</span></p></div>');
-					oc++;
-				} else nav = nav.concat(e);
-			});
+		if (a.elements) a.elements.forEach(e => (typeof e == 'object') ? (nav = nav.concat('<li class="navnav' + (oc == 0 ? ' current' : '') + '" id="' + (e.name || '') + '"><a onclick="divShow(\'' + (e.name || '') + '\')">' + (e.name || '') + '</a></li>'), container = container.concat('<div id="' + (e.name || '') + '" class="element' + (oc == 0 ? '' : ' dispf') + '">' + (r => r.map(d => '<img data-src="' + d.replace(/(\/thumbnail\/)/g, '/') + '" src="' + d + '" class="lazyload" />').join('\n'))(e.images || []) + '<p><span class="center info">' + (e.description || '') + '</span></p></div>'), oc++) : (nav = nav.concat(e)));
 
 		document.body.innerHTML = '<div class="nav"><a href="" id="title" style="outline: 0; text-decoration: none; font-size: smaller; padding-bottom: 5px; padding-left: 5px;">JULES NADEAU</a><nav><a id="openNav" onclick="Nav.open()" style="border-radius: 15%; border: 1px solid #777777;">&#9776;</a><div class="sidenav"><ul class="no-bullet" style="font-size: smaller;"><a id="closeNav" onclick="Nav.close()">&times;</a>\n' + nav.join('\n') + '\n</ul></div></nav></div>\n' + '<div class="container">\n' + container.join('\n') + '\n</div>\n';
 
