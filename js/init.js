3b017e7fe6ae94185e33d2e3db486a7b8950a38a
Paul-Louis <paullouismasca@gmail.com>
2018-03-08 11:40:15 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -74,7 +74,7 @@
 					description: `<p style="text-align:left;right:0;line-height:200%;width:90%;">
 	Salut!<br />
 	<br />
-	Étudiant montréalais de ` + (new Date().getFullYear() - new Date("10 01 1999").getFullYear()) + ` ans et photographe à mes heures, 
+	Étudiant montréalais de ` + (new Date().getFullYear() - new Date(1999, 9, 1, 12, 0, 0, 0).getFullYear()) + ` ans et photographe à mes heures, 
 	je vous invite à suivre dans mon aventure argentique à travers votre écran.<br />
 	Ce site regroupe l'enssemble de mes roulaux dans un ordre chronologique.
 	<br />
