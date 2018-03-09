019beeceb761c3f8a3c3304506918af284e406ae
Paul-Louis <paullouismasca@gmail.com>
2018-03-08 20:42:09 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -79,20 +79,15 @@
 					name: 'À propos', 
 					images: [], 
 					description: `<p style="text-align:left;right:0;line-height:200%;width:90%;">
-	Salut!<br />
-	<br />
-	Étudiant montréalais de ` + (new Date().getFullYear() - new Date(1999, 9, 22, 12, 0, 0, 0).getFullYear()) + ` ans et photographe à mes heures, 
-	je vous invite à suivre dans mon aventure argentique à travers votre écran.<br />
-	Ce site regroupe l'enssemble de mes roulaux dans un ordre chronologique.
-	<br />
-	Si vous avez des questions ou désirez tout simplement dire bonjour, 
-	n’hésitez pas à me contacter!<br />
-	<br />
-	Pour toutes questions/requêtes, 
-	contactez moi au:<br />
-	<a href="mailto:julesjulesnadeau@yahoo.ca">
-		julesjulesnadeau@yahoo.ca
-	</a>
+Salut!<br /><br />
+Étudiant montréalais de ` + (new Date().getFullYear() - new Date(1999, 9, 22, 12, 0, 0, 0).getFullYear()) + ` ans et photographe à mes heures, 
+je vous invite à suivre dans mon aventure argentique à travers votre écran.<br />
+Ce site regroupe l'enssemble de mes roulaux dans un ordre chronologique.<br />
+Si vous avez des questions ou désirez tout simplement dire bonjour, 
+n’hésitez pas à me contacter!<br /><br />
+Pour toutes questions/requêtes, 
+contactez moi au:<br />
+<a href="mailto:julesjulesnadeau@yahoo.ca">julesjulesnadeau@yahoo.ca</a>
 </p>`
 				}, 
 				'<li class="blank-element"></li>', 
